db8615b6107cee8cfa7fad4c0af6d4ad035785e0
wliang <wliang@infervision.com>
2021-12-14 01:50:49 +0800

    [add] 新增qiankun实现，已可在开发环境中使用

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { registerMicroApps, start } from 'qiankun';
 import './App.css';
+// 注册子应用
+registerMicroApps([
+  {
+    name: 'react-sub',
+    entry: process.env.REACT_APP_REACT_SUB,
+    container: '#container',
+    activeRule: '/react',
+  },
+  {
+    name: 'vue-sub',
+    entry: process.env.REACT_APP_VUE_SUB,
+    container: '#container',
+    activeRule: '/vue',
+  },
+]);
+// 启动 qiankun
+start();
 
 function App() {
+  const [activeId, setActiveId] = useState(1);
+  console.log('lll', process.env)
+
+  // 修改默认布局，增加子应用挂在容器“＃container”
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <header className="app_header"></header>
+      <div className="app_body">
+        <aside className="menu">
+          <div className={`menu_button ${activeId === 1 && 'active'}`} onClick={() => setActiveId(1)}>react-sub</div>
+          <div className={`menu_button ${activeId === 2 && 'active'}`} onClick={() => setActiveId(2)}>vue-sub</div>
+        </aside>
+        <div id="container" className="content"></div>
+      </div>
     </div>
   );
 }
