1d08ad0a8228d4ded2a0104df968495504534acb
wliang <wliang@infervision.com>
2021-12-26 23:32:06 +0800

    [add] 部署修改

--- deploy.js
+++ deploy.js
@@ -4,6 +4,6 @@ const tagReg = /tag=(.+)/;
 const tag = process.argv[2].match(tagReg)[1];
 
 exec(
-  `docker build --platform linux/amd64 -t wangliang4468/qiankun:react-main-${tag} .`
+  `docker build -t wangliang4468/qiankun:react-main-${tag} .`
 );
 exec(`docker push wangliang4468/qiankun:react-main-${tag}`);
